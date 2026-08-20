6715a2739ebffccb08ba69046088e9052e084e4d
AnanyaMohapatra18 <ananyamohapatra822@gmail.com>
2026-08-20 23:44:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Todos from "./pages/Todos.jsx";
 
+
 function Protected({ children }) {
   const { user } = useAuth();
   return user ? children : <Navigate to="/login" replace />;
